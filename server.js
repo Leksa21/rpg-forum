fc2c080fd44e9f3d703dc9d2c5e03e310324f4dd
Aleksa <radovanovic.aleksa21@gmail.com>
2026-06-07 20:21:44 +0200

    fix: express 5 wildcard route syntax for SPA fallback

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use('/', indexRouter);
 if (process.env.NODE_ENV === 'production') {
   const clientBuild = path.join(__dirname, 'client', 'dist');
   app.use(express.static(clientBuild));
-  app.get('*', (req, res) => {
+  app.get('/{*path}', (req, res) => {
     res.sendFile(path.join(clientBuild, 'index.html'));
   });
 }
